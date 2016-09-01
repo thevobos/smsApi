406db96ab68570690d634615e611e9847dc8598c
cengiz akcan <cengiz@polotasarim.com>
2016-09-01 17:28:05 +0200

    Update

---
+++ ileti merkezi api class/Example.php
@@ -0,0 +1,53 @@
+<?php 
+/*
+*
+*	AUTHOR   : Cengiz Akcan
+*	Mail     : cengiz@polotasarim.com
+*   Project  : Ileti merkezi basic api class 
+*	Github	 : github.com/cengizakcan1996
+*	Facebook : fb.com/cengizakcan1996
+*   Gsm      : 05413509697
+*
+**/
+
+
+        include("Iletimerkezi.php");
+
+        $Iletimerkezi = new Iletimerkezi();
+
+		/*
+			-addnumber(" NUMARA ");
+			
+			Bu parametre çogaltılarak toplu sms atılabilir.
+			
+			
+		*/
+		
+        // Klasik Normal & Toplu Sms Yollama
+        $Sms = $Iletimerkezi
+				->url("send")
+				->username(" KULLANICI ADI ")
+				->password(" ŞİFRE ")
+				->title(" SMS BAŞLIGI ")
+				->addnumber(" NUMARA EKLE ")
+				->text(" GÖNDERİLECEK MESAJ ")
+				->sms()
+				->send();
+		
+		// Çıktı
+		print_r($Sms);
+
+
+        // Bakiye Sorgulama
+        $Status = $Iletimerkezi
+					->url("status")
+					->username(" KULLANICI ADI ")
+					->password(" ŞİFRE ")
+					->status()
+					->send();
+		// Çıktı
+		print_r($Status);
+			
+			
+
+?>
---
+++ ileti merkezi api class/Iletimerkezi.php
@@ -0,0 +1,185 @@
+<?php
+
+class Iletimerkezi
+{
+
+
+    /*
+     * Atanan değişkenler
+     * */
+    public $username        = "";
+    public $password        = "";
+    public $title           = "";
+    public $text           = "";
+    public $numbers         = array();
+    public $numbersexp      = array();
+    public $date            = "";
+    public $xml             = "";
+    public $url             = "";
+    public $header          = array('Content-Type: text/xml');
+
+
+    /*
+     * @ Mixed
+     * # Bu fonksiyon bakiye durumu ve sms gönderimi için seçim yapılması içindir.
+     * */
+    public function url($url = "send"){
+
+        if($url == "send"){
+
+            $this->url =   "http://api.iletimerkezi.com/v1/send-sms";
+
+        }
+        else if ($url = "status"){
+
+            $this->url = "http://api.iletimerkezi.com/v1/get-balance";
+        }
+
+        return $this;
+
+
+    }
+
+    /*
+     * @ Mixed
+     * # Kullanıcı adınız - ( GSM NO )
+     * */
+    public function username($username = ""){
+
+        $this->username =   $username;
+        return $this;
+
+    }
+
+    /*
+     * @ Mixed
+     * # Kullanıcı şifreniz.
+     * */
+    public function password($password = ""){
+
+        $this->password =   $password;
+        return $this;
+
+    }
+
+    /*
+     * @ Mixed
+     * # iletimerkezi.com'da kayıtlı sms başlığınızı gireceksiniz.
+     * */
+    public function title($title = ""){
+
+        $this->title    =   $title;
+        return $this;
+
+    }
+
+    /*
+     * @ Mixed
+     * # Gönderilecek olan sms bilgisini yazacagınız fonksiyon
+     * */
+    public function text($text = ""){
+
+        $this->text    =   $text;
+        return $this;
+
+    }
+
+    /*
+     * @ Mixed
+     * # İleri tarihli bir sms atılacaksa tarihi girilmelidir örnek olarak GG/AA/YYYY SS:DD formatında olamlıdır
+     * */
+    public function date($date = ""){
+
+        $this->date = $date;
+        return $this;
+
+    }
+
+    /*
+     * @ Mixed
+     * Bu alan çoklu numara girişi yapabilirsiniz. bu alanı sadece sms gönderirken kullanabilirsiniz.
+     * */
+    public function addnumber($number = null){
+
+        $this->numbers[] = [$number];
+        return $this;
+
+    }
+
+    /* Sistem Numara Listesi */
+    public function numberlist(){
+
+        $List = "";
+        foreach($this->numbers as $number){
+
+            $List .= "<number>{$number[0]}</number>";
+
+        }
+
+        return $List;
+
+    }
+
+    /* Sms Gönderimi */
+    public function sms(){
+
+
+
+        $this->xml =  <<<EOS
+<request>
+     <authentication>
+         <username>{$this->username}</username>
+         <password>{$this->password}</password>
+     </authentication>
+
+     <order>
+         <sender>{$this->title}</sender>
+         <sendDateTime>{$this->date}</sendDateTime>
+         <message>
+             <text>{$this->text}</text>
+             <receipents>{$this->numberlist() }</receipents>
+         </message>
+     </order>
+</request>
+EOS;
+        return $this;
+
+    }
+
+    /* Bakiye Sorgulama */
+    public function status(){
+
+        $this->xml =  <<<EOS
+<request>
+     <authentication>
+         <username>{$this->username}</username>
+         <password>{$this->password}</password>
+     </authentication>
+</request>
+EOS;
+        return $this;
+
+    }
+
+    /* Post Fonksiyonu */
+    public function send(){
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$this->url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->xml);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,$this->header);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 120);
+
+        return simplexml_load_string(curl_exec($ch));
+
+
+
+
+    }
+
+
+}
